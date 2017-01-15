74d79458f5eda8ee9234756036e3182325cc10b7
redside100 <andrewpeng00@gmail.com>
2017-01-14 20:10:40 -0500

    Added centered text writing method

--- app/src/main/java/com/redside/rngquest/gameobjects/CoreManager.java
+++ app/src/main/java/com/redside/rngquest/gameobjects/CoreManager.java
@@ -3,13 +3,13 @@ package com.redside.rngquest.gameobjects;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 
 public class CoreManager{
     public int width;
     public int height;
-    private int xoff = 530;
-    private int yoff = 250;
     private int tick = 0;
+    private int xoff = 0;
     boolean left = true;
     public CoreManager(){
         init();
@@ -20,14 +20,14 @@ public class CoreManager{
         // Just testing a left and right moving animation for the text
         tick++;
         if (left){
-            xoff += 3;
-            if (tick == 50){
+            xoff += 1;
+            if (tick == 20){
                 tick = 0;
                 left = false;
             }
         }else{
-            xoff -= 3;
-            if (tick == 50){
+            xoff -= 1;
+            if (tick == 20){
                 tick = 0;
                 left = true;
             }
@@ -38,6 +38,13 @@ public class CoreManager{
         radius = 100;
         paint.setColor(Color.parseColor("#000000"));
         canvas.drawCircle(width / 2, height / 2, radius, paint);
-        canvas.drawText("New font WHOAAA", width / 2 - xoff, height / 2 + yoff, paint);
+        drawCenteredText("New font WHOAAA", canvas, width / 2 - xoff, height / 2 + 300, paint);
+    }
+    public void drawCenteredText(String text, Canvas canvas, int x, int y, Paint paint){
+        Rect bounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bounds);
+        x -= bounds.width() / 2;
+        y -= bounds.height() / 2;
+        canvas.drawText(text, x, y, paint);
     }
 }
